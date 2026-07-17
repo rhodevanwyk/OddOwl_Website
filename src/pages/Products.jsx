4c757bfe6f4b52a414d7358953cecd91ebaeeea1
Skye Lexus Delport <skye.lexus205@gmail.com>
2026-07-17 17:01:04 +0200

    Small Debug

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -4,9 +4,9 @@ import navLogo from "../assets/favicon.png";
 import product1 from "../assets/product1.png";
 import product2 from "../assets/product2.png";
 import product3 from "../assets/product3.png";
-import product4 from "../assets/product3.png";
-import product5 from "../assets/product3.png";
-import product6 from "../assets/product3.png";
+import product4 from "../assets/product4.png";
+import product5 from "../assets/product5.png";
+import product6 from "../assets/product6.png";
 import "../index.css";
 
 const WHATSAPP_NUMBER = "27768519561";
